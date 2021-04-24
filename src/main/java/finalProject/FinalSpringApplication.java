f367afb45e961211eef73375bb7233857bca24ab
Aleksandr Kremlev <kremlsa@yandex.ru>
2021-04-24 23:23:05 +0700

    Настроено логирование, исправлена кодировка в surefire-plugin.

--- src/main/java/finalProject/FinalSpringApplication.java
+++ src/main/java/finalProject/FinalSpringApplication.java
@@ -2,6 +2,12 @@ package finalProject;
 
 import org.springframework.boot.SpringApplication;
 
+/**
+ * Класс для звпуска SpringBoot
+ *
+ * @author Alexander Kremlev
+ * @version 1.0
+ */
 public class FinalSpringApplication {
 
     public static void main(String[] args) {
