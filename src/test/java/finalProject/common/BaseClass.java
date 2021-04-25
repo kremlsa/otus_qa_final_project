571fcfba7263ad896f0c97a3131923aad9d8159e
Aleksandr Kremlev <kremlsa@yandex.ru>
2021-04-25 18:25:47 +0700

    Настроено логирование, исправлена кодировка в surefire-plugin.

--- src/test/java/finalProject/common/BaseClass.java
+++ src/test/java/finalProject/common/BaseClass.java
@@ -2,7 +2,6 @@ package finalProject.common;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,16 +12,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class BaseClass {
-    public static WebDriver driver;
     public static final Logger logger = LogManager.getLogger();
-
-    public static void setDriver(WebDriver driver) {
-        BaseClass.driver = driver;
-    }
-
-    public static WebDriver getDriver() {
-        return driver;
-    }
 
     public static Logger getLogger() {
         return logger;
